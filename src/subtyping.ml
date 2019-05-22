54a2f343e8a69cf3242ed2f059a88c8fb5c2a5d2
iambrj <joshibharathiramana@gmail.com>
2019-05-23 00:32:43 +0530

    added subtype function

--- src/subtyping.ml
+++ src/subtyping.ml
@@ -10,3 +10,21 @@ type term =
     |    TmVar of info * int * int
     |    TmAbs of info * string * ty * term
     |    TmApp of info * term * term
+
+
+let rec subtype tyS tyT =
+   (=) tyS tyT ||
+   match (tyS, tyT) with
+               (TyRecord(fS), TyRecord(fT))    ->
+                   List.for_all
+                   (fun (li, tyTi)    ->
+                       try let tySi = List.assoc li fS in
+                           subtype tySi tyTi
+                       with Not_found    ->    false)
+               fT
+           |    (_, TyTop)    ->
+                   true
+           |    (TyArr(tyS1, tyS2), TyArr(tyT1, tyT2))    ->
+                   (subtype tyT1 tyS1) && (subtype tyS2 tyT2)
+           |    (_, _)    ->
+                   false
